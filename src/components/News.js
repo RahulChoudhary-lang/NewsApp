849857315c3f1379a8f2f03af2ec2809ad7d1eff
Rahul Choudhary <choudharyrahul1711@gmail.com>
2022-08-15 22:29:24 +0530

    component structure

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export default class news extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+        <NewsItem />
+      </div>
+    );
+  }
+}
