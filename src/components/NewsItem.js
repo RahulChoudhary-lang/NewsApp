2461a9811300eb7af81a4a9662099dbf9c6ba6fa
Rahul Choudhary <choudharyrahul1711@gmail.com>
2022-08-16 21:33:01 +0530

    fetching API from news api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>These is NewsItem</div>;
+    let { title, description } = this.props;
+
+    return (
+      <div>
+        <div className="card">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a className="btn btn-primary" href="/">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
